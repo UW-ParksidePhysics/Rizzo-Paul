911588fc5e23dd8caa75edc9f2e9a1a05e49bc10
PaulieRizzo <60677234+PaulieRizzo@users.noreply.github.com>
2020-02-27 15:29:04 -0600

    Add files via upload

---
+++ 1.5_1liter_12.py
@@ -0,0 +1,20 @@
+Volume=1
+D_Iron=7.874
+D_Gasoline=0.7489
+D_Ice=0.92
+D_TheHumanBody=0.985
+D_Silver=10.49
+D_Platinum=21.45
+
+M_Iron=D_Iron*Volume
+M_Gasoline=D_Gasoline*Volume
+M_Ice=D_Ice*Volume
+M_TheHumanBody=D_TheHumanBody*Volume
+M_Silver=D_Silver*Volume
+M_Platinum=D_Platinum*Volume
+
+print("Mass of Iron = ",M_Iron)
+print("Mass of Gasoline =",
+print("Mass of
+print("Mass of
+
